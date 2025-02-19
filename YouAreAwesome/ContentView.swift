347af01bb9374b6c9ec4a7aa33286b8764556dad
Tim Ruhl <timjruhl@gmail.com>
2025-02-18 19:43:16 -0500

    Used variables insted

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "I am a programmer"
     var body: some View {
         
         VStack {
@@ -15,10 +16,13 @@ struct ContentView: View {
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("I am an app devloper!")
+            Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+            Button("Click Me!") {
+                message = "Awesome!"
+            }
         }
         .padding()
     
